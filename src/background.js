65054aaca6990a8914d5c58e1f8da703fc681f9c
Jinli Xiao <jinli.xiao@nyu.edu>
2023-02-11 22:00:26 -0500

    delete entry on tab close and add documentations

--- src/background.js
+++ src/background.js
@@ -1,21 +1,41 @@
 "use strict";
 
+/**
+ * pdfLinks
+ *
+ * A map that stores the last requested PDF link for each tab. The keys in the
+ * map are tab IDs, and the values are the corresponding PDF links.
+ *
+ * @type {Map<number, string>}
+ */
 var pdfLinks = new Map();
 
-/*
- * Handler function for the webRequest.onBeforeSendHeaders event
- * Filters out requests that are not PDFs, and extracts the PDF link
- * from the request headers
+/**
+ * extractPDFLink(e)
+ *
+ * This function is a handler for the webRequest.onBeforeSendHeaders event. It
+ * filters out requests that are not PDFs, and extracts the PDF link from the
+ * request headers. The PDF link and the tab ID associated with the request are
+ * stored in a map for later use.
+ *
+ * @param {Object} request - The request headers object
+ * @return {Object} - An empty object
  */
-function extractPDFLink(e) {
-  if (e.url.includes(".pdf")) {
-    pdfLinks.set(e.tabId, e.url);
+function extractPDFLink(request) {
+  if (request.url.includes(".pdf")) {
+    pdfLinks.set(request.tabId, request.url);
   }
   return {};
 }
 
-/*
- * Display the PDF embedded in the page
+/**
+ * displayPDF()
+ *
+ * This function retrieves the last requested PDF link associated with the
+ * currently active tab and opens it in a new tab. If there is no PDF link
+ * associated with the active tab, the function does nothing.
+ *
+ * @return {void}
  */
 function displayPDF() {
   browser.tabs
@@ -35,10 +55,17 @@ function displayPDF() {
     });
 }
 
+// Listen for requests to PDFs
 browser.webRequest.onBeforeSendHeaders.addListener(
   extractPDFLink,
   { urls: ["*://*/*"] },
   ["blocking", "requestHeaders"]
 );
 
+// Fire displayPDF() when the browser action is clicked
 browser.browserAction.onClicked.addListener(displayPDF);
+
+// Remove PDF links from the map when tabs are closed
+browser.tabs.onRemoved.addListener((tabId) => {
+  pdfLinks.delete(tabId);
+});
